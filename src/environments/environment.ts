21a0e829b6cb76a55ef22c5c0038ac17800b9c5c
Esteban Botero <estebanbotero1036@gmail.com>
2020-02-12 11:23:32 -0500

    Modificacion boton generar pdf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   urlRaiz: 'https://enovelsoluciones.sharepoint.com',
-  urlWeb: 'https://enovelsoluciones.sharepoint.com/sites/IntranetAraujo/servicios', 
+  urlWebServicio: 'https://enovelsoluciones.sharepoint.com/sites/IntranetAraujo/servicios', 
   urlWebGH: 'https://enovelsoluciones.sharepoint.com/sites/IntranetAraujo/Gestion-humana', 
   urlReltativa: 'sites/Intranet/Servicios',  
   ListaEmpleados: 'Empleados',
