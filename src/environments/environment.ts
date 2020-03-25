cb05e2b8e35ce0f387b83fb1624abe5640eb1bc4
Esteban Botero <estebanbotero1036@gmail.com>
2020-03-25 17:06:23 -0500

    modificacion para montaje a pruebas Araujo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@
 export const environment = {
   production: false,
   urlRaiz: 'https://aribasas.sharepoint.com',
-  urlWebServicio: 'https://aribasas.sharepoint.com/sites/Intranet/Servicios', 
-  urlWebGH: 'https://aribasas.sharepoint.com/sites/Intranet/Gestion-humana', 
-  urlReltativa: 'sites/Intranet/Servicios',  
+  urlWebServicio: 'https://aribasas.sharepoint.com/sites/IntranetPruebas/Servicios', 
+  urlWebGH: 'https://aribasas.sharepoint.com/sites/IntranetPruebas/Gestion-humana', 
+  urlReltativa: 'sites/IntranetPruebas/Servicios',  
   ListaEmpleados: 'Empleados',
   ListaServicios: 'Servicios',
   ListaSolicitudPermisos: 'SolicitudPermisos',
